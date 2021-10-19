2b9657ef94fee076678bf7bbaea201b5e540465c
Nigar Ahmadova <61373390+Nigar16@users.noreply.github.com>
2021-10-19 22:37:33 +0400

    Delete general.js

--- src/routers/general.js
+++
@@ -1,7 +0,0 @@
-const router=require("express").Router();
-const generalController=require("../controller/general")
-router.get("/all-products",generalController.productGet);
-router.get("/all-categories",generalController.categoryGet);
-router.post("/add-product",generalController.productAdd);
-
-module.exports=router;
